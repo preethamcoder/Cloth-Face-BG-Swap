87ce7bd62b3b4a362a56c2e2cda2bb7f38e6422e
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2024-04-28 14:37:40 -0400

    Initial setup to get your environment ready

---
+++ setup.sh
@@ -0,0 +1,8 @@
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+pip3 install -q regex tqdm
+pip3 install -q diffusers transformers accelerate scipy
+pip3 install -q -U xformers
+pip3 install -q opencv-python
+pip3 install -q git+https://github.com/facebookresearch/segment-anything.git
+pip3 install -q pycocotools matplotlib onnxruntime onnx
+
